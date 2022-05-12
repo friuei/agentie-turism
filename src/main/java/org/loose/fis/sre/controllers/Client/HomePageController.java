7c9d7aa5f9e795562cc2daa27197ea5770cfb141
Steli <v.stely71@gmail.com>
2022-05-12 19:22:30 +0300

    AT-1-AT-2-HOMEPAGE

---
+++ src/main/java/org/loose/fis/sre/controllers/Client/HomePageController.java
@@ -0,0 +1,81 @@
+package org.loose.fis.sre.controllers.Client;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HomePageController {
+
+    @FXML
+    private Button bookanoffer;
+
+    @FXML
+    private AnchorPane change;
+
+    @FXML
+    private Button homepage;
+
+    @FXML
+    private AnchorPane homepageanc;
+
+    @FXML
+    private Button logout;
+
+    @FXML
+    private Button review;
+
+    @FXML
+    private Button seeRequest;
+
+    @FXML
+    private Button seeoffers;
+
+    @FXML
+    void bookAnOfferOnAction(ActionEvent event) throws IOException {
+        AnchorPane pane=FXMLLoader.load(getClass().getClassLoader().getResource("book_an_offer.fxml"));
+        change.getChildren().setAll(pane);
+    }
+
+    @FXML
+    void homePageOnAction(ActionEvent event) throws IOException {
+        AnchorPane pane= FXMLLoader.load(getClass().getClassLoader().getResource("instructors_client.fxml"));
+        change.getChildren().setAll(pane);
+    }
+
+    @FXML
+    void logOutOnAction(ActionEvent event) throws IOException {
+        Stage stage;
+        Parent root;
+        stage = (Stage) logout.getScene().getWindow();
+        root=FXMLLoader.load(getClass().getClassLoader().getResource("login.fxml"));
+
+        stage.setScene(new Scene(root, 600, 450));
+        stage.show();
+
+    }
+
+    @FXML
+    void reviewAnOffer(ActionEvent event) throws IOException {
+        AnchorPane pane=FXMLLoader.load(getClass().getClassLoader().getResource("instructors_client.fxml"));
+        change.getChildren().setAll(pane);
+    }
+
+    @FXML
+    void seeOffersOnAction(ActionEvent event) throws IOException {
+        AnchorPane pane=FXMLLoader.load(getClass().getClassLoader().getResource("instructors_client.fxml"));
+        change.getChildren().setAll(pane);
+    }
+
+    @FXML
+    void seeRequestOnAction(ActionEvent event) {
+
+    }
+
+}
